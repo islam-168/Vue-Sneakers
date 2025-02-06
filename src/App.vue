d832dbdc0c3ca23914a7b23d2344f794787af774
Isl <islamov.islamiddin@gmail.com>
2025-02-06 07:40:51 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+import Header from "@/components/Header.vue";
+</script>
+
+<template>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-8">
+    <Header />
+  </div>
+</template>
